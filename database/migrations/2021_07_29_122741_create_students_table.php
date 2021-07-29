92346c1a5f3010a8e7f4bbbe35b79eb6f2ef9ad5
shruthijuno <shruthip056@gmail.com>
2021-07-29 18:29:17 +0530

    migrations

---
+++ database/migrations/2021_07_29_122741_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email')->unique();
+            $table->unsignedBigInteger('course');
+            $table->foreign('course')->references('id')->on('courses');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
